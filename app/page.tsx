f3991851899dda7dd6e55ad340f234fcc15f0d4c
Kapish <kapishsingh2209@gmail.com>
2023-09-01 19:30:00 +0530

    spel

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="text-center mt-20 animate-bounce">
       <Link className="btn " href={"/1"}>
-        Click OnM Mr To Start
+        Click OnM Me To Start
       </Link>{" "}
     </div>
   );
